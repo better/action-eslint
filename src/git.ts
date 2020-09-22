adad4ad551a2e649c5257a06d74637f4c858aab7
Jay Dey <jdey@better.com>
2020-09-22 08:57:39 -0400

    add diff parsing

---
+++ src/git.ts
@@ -0,0 +1,46 @@
+const getChangedLinesFromHunk = (hunk) => {
+  let lineNumber = 0;
+  const changedLines = [];
+
+  for (const line of hunk) {
+    if (line.startsWith('@@')) {
+      lineNumber = Number(line.match(/\+([0-9]+)/)[1]);
+      continue;
+    }
+
+    if (!line.startsWith('-')) {
+      if (line.startsWith('+') && lineNumber != 0) {
+        changedLines.push(lineNumber);
+      }
+      lineNumber++;
+    }
+  }
+
+  return changedLines;
+};
+
+const getHunksFromDiff = (str) => {
+  const lines = str.split('\n');
+  const hunks = [];
+  let i = -1;
+
+  for (const line of lines) {
+    if (line.startsWith('diff --git')) {
+      i++;
+      hunks[i] = [];
+    }
+    hunks[i].push(line);
+  }
+
+  return hunks;
+};
+
+export const getChangedLinesByFile = (diff) => {
+  const changedLinesByFile = new Map();
+
+  for (const hunk of getHunksFromDiff(diff)) {
+    const file = hunk[0].split(' ')[3].substring(2);
+    changedLinesByFile.set(file, getChangedLinesFromHunk(hunk));
+  }
+  return changedLinesByFile;
+};
